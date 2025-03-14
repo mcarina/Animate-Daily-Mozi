9a9fb8cdb262d21d19e1981ddefb1a03b59d4a29
mcarina <marciacarina931@gmail.com>
2025-03-14 11:11:23 -0400

    background interativo

---
+++ src/components/navbar.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+export default function Navbar ({ onColorChange }: ColorPickerProps){
+
+    const pastelColors = [
+        { value: "#f8edeb" },
+        { value: "#d0e8f2" },
+        { value: "#d8f3dc" },
+        { value: "#fcf6bd" },
+        { value: "#e0c3fc" },
+    ]
+
+    return(
+        <section className="space-y-4">
+            <div className="grid grid-cols-5 gap-2 w-64 h-64">
+                {pastelColors.map((color) => (
+                <button
+                    key={color.value}
+                    onClick={() => onColorChange(color.value)}
+                    className="w-full aspect-square rounded-md border border-gray-200 transition-transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-primary"
+                    style={{ backgroundColor: color.value }}
+                />
+                ))}
+            </div>    
+        </section>
+    )
+};
